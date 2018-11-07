c05c86b1ba3c25cb74e4ed0cdb1ca0d088424e05
Larissa <larissa95.amaral@gmail.com>
2018-11-07 19:30:23 -0200

    Insere mensagem de erro caso de login incorreto

--- src/main/java/br/usp/poli/configuration/SecurityConfiguration.java
+++ src/main/java/br/usp/poli/configuration/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package br.usp.poli.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -37,7 +40,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 					.logout()
 					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 					.logoutSuccessUrl("/")
-					.permitAll();
+					.permitAll()
+				.and()
+					.sessionManagement()
+					.invalidSessionUrl("/login")
+					.maximumSessions(1)
+			        .maxSessionsPreventsLogin(true)
+			        .sessionRegistry(sessionRegistry());;
 		
 		http.exceptionHandling().accessDeniedPage("/acess-denied");
 		
@@ -55,6 +64,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
  
+    }
+	
+	@Bean
+    public SessionRegistry sessionRegistry() {
+        SessionRegistry sessionRegistry = new SessionRegistryImpl();
+        return sessionRegistry;
     }
 
 	 /* CRIPTOGRAFANDO A SENHA PARA TESTE
--- src/main/java/br/usp/poli/controller/LoginController.java
+++ src/main/java/br/usp/poli/controller/LoginController.java
@@ -1,7 +1,10 @@
 package br.usp.poli.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,9 +23,19 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/login")
-	public ModelAndView login(){
-		ModelAndView modelAndView = new ModelAndView("Login");
-		return modelAndView;
+	public ModelAndView login(@AuthenticationPrincipal User user, @RequestParam(required=false) String error){
+		
+		if(user!=null) {
+			return new ModelAndView("redirect:/menu");
+		}
+		
+		ModelAndView mav = new ModelAndView("Login");
+		
+		if(error != null) {
+			mav.addObject("errorMessage", "Invalid credentials");
+		}
+		
+		return mav;
 	}
 	
 	@RequestMapping(value="/acess-denied")
